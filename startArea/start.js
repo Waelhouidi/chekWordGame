ce5be928d29af11d256de785a6719df65889484b
Waelhouidi <waelhwidi0@gmail.com>
2024-05-12 01:02:33 +0100

    fix bugs

--- startArea/start.js
+++ startArea/start.js
@@ -1,17 +1,15 @@
-  // Get references to the images
   const hangmanImg = document.getElementById('hangmanImg');
   const wordlImg = document.getElementById('wordlImg');
 
-  // Function to open an external page when an image is clicked
-  function openExternalPage(url) {
-    window.open(url, '_blank'); // Open the URL in a new tab
+  function goToGame(url) {
+    window.location.href = url;
   }
 
-  // Add click event listeners to the images
   hangmanImg.addEventListener('click', function() {
-    openExternalPage('../hangman/hangman.html');
+    goToGame('../hangman/hangman.html');
   });
 
   wordlImg.addEventListener('click', function() {
-    openExternalPage('../gessArea/gess.html')
+    goToGame('../gessArea/gess.html');
   });
+
